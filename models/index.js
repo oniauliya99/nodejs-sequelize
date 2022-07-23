dd0a5d672a6b98a1e1612790c22e195052778744
oniauliya99 <oniauliya99@gmail.com>
2022-07-23 11:17:49 +0700

    create productModel and reviewModel

--- models/index.js
+++ models/index.js
@@ -36,3 +36,10 @@ db.Sequelize = Sequelize
 db.sequelize = sequelize
 
 db.products = require('./productModel.js')(sequelize, DataTypes)
+db.reviews = require('./reviewModel.js')(sequelize, DataTypes)
+
+db.sequelize.sync({
+    force: false
+}).then(() => {
+    console.log('yes, resync done')
+})
